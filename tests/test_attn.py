4118df97b6655bebdc320aa177741d837aac0356
lucidrains <lucidrains@gmail.com>
2025-07-06 07:20:15 -0700

    full strassen attention

--- tests/test_attn.py
+++ tests/test_attn.py
@@ -2,4 +2,18 @@ import pytest
 import torch
 
 def test_attn():
-    assert True
+    from strassen_attention.strassen_attention import strassen_attend
+
+    q = torch.randn(1, 8, 32, 16)
+    k = torch.randn(1, 8, 32, 16)
+    v = torch.randn(1, 8, 32, 16)
+
+    attended = strassen_attend(
+        q,
+        k,
+        k.clone(),
+        v,
+        v.clone()
+    )
+
+    assert attended.shape == (1, 8, 32, 16)
